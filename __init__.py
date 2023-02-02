1a460e7c5af4109980bd5e547de07b0fbb1277fc
Jaafar-G <jaafar309@hotmail.com>
2023-02-02 14:47:39 +0000

    "adding sql update 1/2/23"

--- __init__.py
+++ __init__.py
@@ -7,15 +7,21 @@ config = {
         }
 
 app = Flask(__name__)
+app.secret_key = "jdsfau374bb3f734hf4f37434yf9"
 
 @app.route("/")  # this sets the route to this page
 def home():
-    return jsonify({'Msage': "Testing"})
+    return jsonify({'Message': "Testing"})
 
 
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
     return jsonify({'ip': request.access_route}), 200
+
+
+@app.route("/get_my_date", methods=["GET"])
+def get_my_date():
+        return jsonify({'Date': request.date}), 200
 
 
 if __name__ == "__main__":
