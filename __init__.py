94aa89ec940f192c2722f345a51fd1d9d513e50a
Jaafar-G <jaafar309@hotmail.com>
2023-02-03 08:17:01 -0600

    adding sqlite changes to log client ip pt.1

--- __init__.py
+++ __init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import sqlite3
 
 config = {
-        "DEBUG": True # run app in debug mode
+        "DEBUG": True  # run app in debug mode
         }
+
 
 app = Flask(__name__)
 app.secret_key = "jdsfau374bb3f734hf4f37434yf9"
+
 
 @app.route("/")  # this sets the route to this page
 def home():
@@ -19,11 +22,31 @@ def get_my_ip():
     return jsonify({'ip': request.access_route}), 200
 
 
+# Creates a connection to the database names Client_req_logs
+conn = sqlite3.connect("IP_Req_Logs.db")
+print("opened database successfully");
+
+# Creates a cursor object that allows interaction with database and add records
+cursor = conn.cursor()
+
+# Creates a table
+#cursor.execute (
+ #   "INSERT INTO ips VALUES"
+#)
+
+print("IP table created successfully");
+
+# commit our command
+conn.commit()
+
+# close our connection
+conn.close()
+
+
 @app.route("/get_my_date", methods=["GET"])
 def get_my_date():
-        return jsonify({'Date': request.date}), 200
+    return jsonify({'Date': request.date}), 200
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
--- db.py
+++ db.py
@@ -1,11 +1,25 @@
 import sqlite3
+from flask import *
+
+client_ip = str(request.access_route)
 
 # Creates a connection to the database names Client_req_logs
-conn = sqlite3.connect("Client_req_logs.db")
+conn = sqlite3.connect("IP_Req_Logs.db")
+print("opened database successfully");
 
 # Creates a cursor object that allows interaction with database and add records
 cursor = conn.cursor()
 
 # Creates a table 
-cursor.execute("""CREATE TABLE ips 
+cursor.execute("""CREATE TABLE client_ip (
+    client_ip TEXT PRIMARY KEY
+    
     )
+    """)
+print("IP table created successfully");
+
+# commit our command
+conn.commit()
+
+# close our connection
+conn.close()
